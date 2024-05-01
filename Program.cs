2ed965d2a47d63aef5f04c4924683283ee95c511
Kyle Laverty <k.laverty@gmail.com>
2024-05-01 16:27:56 -0400

    Some minor updates to program.cs layout and added global suppressions

--- Program.cs
+++ Program.cs
@@ -1,33 +1,31 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 using FastEndpoints.Swagger;
 using Learning.FastEndpoionts.Services;
 using Serilog;
 using Serilog.Events;
-using Serilog.Formatting.Compact;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .CreateBootstrapLogger();
+const string _applicationName = "Learning - FastEndpoints";
+
+Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+
+InitializeBootstrapLogger();
 
 try
 {
-    Log.Information("Starting web application");
+    Log.Information("Main - Init {ApplicationName}", _applicationName);
+    Log.Information("Executing in user context: {User}", Environment.UserName);
 
-    var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(new WebApplicationOptions{
+        Args = args
+    });
 
-    builder.Services.AddSerilog((services, loggerConfig) =>
-        {
-            Log.Information("Starting Full Log Configuration");
-            loggerConfig
-                .ReadFrom.Configuration(builder.Configuration)
-                .ReadFrom.Services(services)
-                .Enrich.FromLogContext();
-        }
+    builder.Services.AddSerilog((services, loggerConfig) => 
+        loggerConfig
+            .ReadFrom.Configuration(builder.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
     );
 
     builder.Services
@@ -44,8 +42,8 @@ try
 
             o.DocumentSettings = s =>
             {
-                s.Title = "My Testing FastEndpoints API";
-                s.Description = "My API description";
+                s.Title = $"{_applicationName} API";
+                s.Description = $"My {_applicationName} API description";
                 s.DocumentName = "v1.0";
                 s.Version = "v1.0";
             };
@@ -64,14 +62,31 @@ try
     .UseSwaggerGen();
 
     await app.RunAsync();
-    return 0;
 }
 catch (Exception ex)
 {
+    // Catch errors in project setup that result in an unusable system.
     Log.Fatal(ex, "Host terminated unexpectedly");
-    return 1;
 }
 finally
 {
+    Log.Information("Main - Shutdown {ApplicationName}", _applicationName);
+
+    // Ensure to flush and stop internal timers/threads before application-exit
+    // (Avoid segmentation fault on Linux).
     Log.CloseAndFlush();
+}
+
+
+
+static void InitializeBootstrapLogger()
+{
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateBootstrapLogger();
 }
